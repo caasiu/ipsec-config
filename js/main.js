0371c2f7e19e21096670af87338441fb5e68b10e
caasiu <Rogers619952467@gmail.com>
2016-09-07 14:11:43 +0000

    add button animate

--- js/main.js
+++ js/main.js
@@ -1,13 +1,74 @@
 
 $(document).ready(function() {
+
+    //init
+    $("#installation-os-u").hide();
+    $("#build-kvm").hide();
+    $("#transfer-kvm").hide();
+    $("#startup-os-u").hide();
+
     $("#installation-btn-l").click(function() {
-        $("#installation-os-c").fadeIn();
         $("#installation-os-u").hide(0);
+        $("#installation-os-c").addClass('animated fadeInUp');
+        $("#installation-os-c").show();
+        $(this).addClass('btn-press');
+        $("#installation-btn-r").removeClass('btn-press');
     });
 
     $("#installation-btn-r").click(function() {
         $("#installation-os-c").hide(0);
-        $("#installation-os-u").fadeIn();
+        $("#installation-os-u").addClass('animated fadeInUp');
+        $("#installation-os-u").show();
+        $(this).addClass('btn-press');
+        $("#installation-btn-l").removeClass('btn-press');
+    });
+
+    $("#build-btn-l").click(function() {
+        $("#build-openvz").hide(0);
+        $("#build-kvm").addClass('animated fadeInUp');
+        $("#build-kvm").show();
+        $(this).addClass('btn-press');
+        $("#build-btn-r").removeClass('btn-press');
+    });
+
+    $("#build-btn-r").click(function() {
+        $("#build-kvm").hide(0);
+        $("#build-openvz").addClass('animated fadeInUp');
+        $("#build-openvz").show();
+        $(this).addClass('btn-press');
+        $("#build-btn-l").removeClass('btn-press');
+    });
+
+    $("#transfer-btn-l").click(function() {
+        $("#transfer-openvz").hide(0);
+        $("#transfer-kvm").addClass('animated fadeInUp');
+        $("#transfer-kvm").show();
+        $(this).addClass('btn-press');
+        $("#transfer-btn-r").removeClass('btn-press');
+    });
+
+    $("#transfer-btn-r").click(function() {
+        $("#transfer-kvm").hide(0);
+        $("#transfer-openvz").addClass('animated fadeInUp');
+        $("#transfer-openvz").show();
+        $(this).addClass('btn-press');
+        $("#transfer-btn-l").removeClass('btn-press');
+    });
+
+    $("#startup-btn-l").click(function() {
+        $("#startup-os-u").hide(0);
+        $("#startup-os-c").addClass('animated fadeInUp');
+        $("#startup-os-c").show();
+        $(this).addClass('btn-press');
+        $("#startup-btn-r").removeClass('btn-press');
+    });
+
+    $("#startup-btn-r").click(function() {
+        $("#startup-os-c").hide(0);
+        $("#startup-os-u").addClass('animated fadeInUp');
+        $("#startup-os-u").show();
+        $(this).addClass('btn-press');
+        $("#startup-btn-l").removeClass('btn-press');
     });
 
 });
